d1c69246b6d7f9b2dd8be615c9bc5f1f985b2672
Uzair Shahid <75163043+uzair1111@users.noreply.github.com>
2024-09-14 17:53:20 +0500

    Create Inverted_half_pyramid.java

---
+++ Inverted_half_pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Inverted_half_pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What should be the height");
+        int height = sc.nextInt();
+
+        System.out.println("Here is you rectangle::");
+        for(int i=height; i>=1; i--){
+            
+            for(int j=i; j>=1; j = j-1){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
