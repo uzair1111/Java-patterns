339bf110504ff229de99c4580dbf004f1a0339b1
Uzair Shahid <75163043+uzair1111@users.noreply.github.com>
2024-09-15 12:06:16 +0500

    Update Half_pyramid.java

--- Half_pyramid.java
+++ Half_pyramid.java
@@ -6,7 +6,7 @@ public class Half_pyramid {
         System.out.println("What should be the height");
         int height = sc.nextInt();
 
-        System.out.println("Here is you rectangle::");
+        System.out.println("Here is your Half pyramid::");
         for(int i=1; i<=height; i++){
             
             for(int j=1; j<=i; j++){
